5a650a8726ca163e4b87e6ee9a17f3d203cf5785
margotgoudard <goudard.margot@hotmail.com>
2024-12-10 12:33:17 +0100

    audit routes

--- routes/auditRoutes.js
+++ routes/auditRoutes.js
@@ -8,5 +8,6 @@ router.get('', auditController.getAudits);
 router.get('/:id_audit', auditController.getAuditById);
 router.put('/:id_audit', auditController.updateAudit);
 router.delete('/:id_audit', auditController.deleteAudit);
+router.get('/audits/user/:id_user', auditController.getAuditsByUserId);
 
 module.exports = router;
