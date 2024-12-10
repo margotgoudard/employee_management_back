1eb5e048006721c26452143fa0ceaf6f1c3efe79
margotgoudard <goudard.margot@hotmail.com>
2024-12-10 10:59:02 +0100

    permission model

---
+++ models/Permission.js
@@ -0,0 +1,25 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/sequelize');
+
+class Permission extends Model {}
+
+Permission.init(
+  {
+    id_permission: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Permission',
+  }
+);
+
+module.exports = Permission;
