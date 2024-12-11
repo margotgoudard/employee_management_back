759a1bff71a050cb52a9675d481bee6564ee6057
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-12-11 12:26:18 +0100

    ajout usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-const User = require('../models/User');
-
-const userController = {
-
-  // Créer un nouvel utilisateur
-  createUser: async (req, res) => {
-    try {
-      const {
-        first_name, last_name, role, mail, phone, password,
-        num_address, street_address, city_address, area_code_address, 
-        region_address, country_address, is_admin, is_sup_admin, last_connected
-      } = req.body;
-
-      
-      const newUser = await User.create({
-        first_name, last_name, role, mail, phone, password,
-        num_address, street_address, city_address, area_code_address, 
-        region_address, country_address, is_admin, is_sup_admin, last_connected
-      });
-
-      
-      return res.status(201).json({ message: 'User created successfully', newUser });
-    } catch (error) {
-      return res.status(500).json({ message: 'Error creating user', error });
-    }
-  }
-};
-=======
 const User = require('../models/User'); // Importation du modèle User
 
 const userController = {
@@ -120,5 +91,4 @@ const userController = {
   },
 };
 
->>>>>>> origin/main
 module.exports = userController;
