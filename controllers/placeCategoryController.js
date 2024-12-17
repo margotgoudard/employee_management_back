440ebf7f0007d9db6e10f83da9fc4e5b79ffdc2e
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-12-17 09:51:30 +0100

    Audit place_category

--- controllers/placeCategoryController.js
+++ controllers/placeCategoryController.js
@@ -1,13 +1,24 @@
 const PlaceCategory = require('../models/PlaceCategory');
+const { createAudit } = require('./auditController');
+
 
 const placeCategoryController = {
-    
+
   // Créer une catégorie de lieux
   createPlaceCategory: async (req, res) => {
     try {
       const { name } = req.body;
+      const userId = req.auth.userId;
 
       const placeCategory = await PlaceCategory.create({ name });
+
+      await createAudit({
+        table_name: 'place_category',
+        action: 'CREATE',
+        old_values: null,
+        new_values: placeCategory.dataValues,
+        userId,
+      });
 
       return res.status(201).json({ message: 'PlaceCategory created successfully', placeCategory });
     } catch (error) {
@@ -48,6 +59,7 @@ const placeCategoryController = {
     try {
       const { id } = req.params;
       const { name } = req.body;
+      const userId = req.auth.userId;
 
       const placeCategory = await PlaceCategory.findByPk(id);
 
@@ -55,7 +67,17 @@ const placeCategoryController = {
         return res.status(404).json({ message: 'PlaceCategory not found' });
       }
 
+      const oldValues = { ...placeCategory.dataValues };
+
       await placeCategory.update({ name });
+
+      await createAudit({
+        table_name: 'place_category',
+        action: 'UPDATE',
+        old_values: oldValues,
+        new_values: placeCategory.dataValues,
+        userId,
+      });
 
       return res.status(200).json({ message: 'PlaceCategory updated successfully', placeCategory });
     } catch (error) {
@@ -67,6 +89,7 @@ const placeCategoryController = {
   deletePlaceCategory: async (req, res) => {
     try {
       const { id } = req.params;
+      const userId = req.auth.userId;
 
       const placeCategory = await PlaceCategory.findByPk(id);
 
@@ -74,7 +97,18 @@ const placeCategoryController = {
         return res.status(404).json({ message: 'PlaceCategory not found' });
       }
 
+      const oldValues = { ...placeCategory.dataValues }; 
+
       await placeCategory.destroy();
+
+      await createAudit({
+        table_name: 'place_category',
+        action: 'DELETE',
+        old_values: oldValues,
+        new_values: null,
+        userId,
+      });
+
       return res.status(200).json({ message: 'PlaceCategory deleted successfully' });
     } catch (error) {
       return res.status(500).json({ message: 'Error deleting place category', error });
