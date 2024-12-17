979d2b0c6d355db437f5f4f211d8d1a82ea950fa
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-12-17 11:29:32 +0100

    Audit sur document

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const documentController = require('../controllers/documentController');
 const upload = require('../middleware/upload'); 
 
-router.post('/', upload.single('document'), documentController.createDocument); 
-router.get('/', documentController.getDocuments);
-router.get('/:id_document', documentController.getDocumentById); 
-router.put('/:id_document', upload.single('document'), documentController.updateDocument); 
-router.delete('/:id_document', documentController.deleteDocument); 
-router.get('/category/:id_category', documentController.getDocumentsByDocumentCategory);
-router.get('/user/:id_user', documentController.getDocumentsByUser);
-router.get('/category/:id_category/user/:id_user', documentController.getDocumentsByDocumentCategoryAndUser);
+const { validateToken } = require('../middleware/auth')
+
+router.post('/',validateToken, upload.single('document'), documentController.createDocument); 
+router.get('/',validateToken, documentController.getDocuments);
+router.get('/:id_document',validateToken, documentController.getDocumentById); 
+router.put('/:id_document',validateToken, upload.single('document'), documentController.updateDocument); 
+router.delete('/:id_document',validateToken, documentController.deleteDocument); 
+router.get('/category/:id_category',validateToken, documentController.getDocumentsByDocumentCategory);
+router.get('/user/:id_user',validateToken, documentController.getDocumentsByUser);
+router.get('/category/:id_category/user/:id_user',validateToken, documentController.getDocumentsByDocumentCategoryAndUser);
 
 module.exports = router;
