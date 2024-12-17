de30385653a3728edd45d41746f16508ff9409e2
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-12-17 09:59:36 +0100

    Audit pour time_slot

--- routes/timeSlotRoutes.js
+++ routes/timeSlotRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const timeSlotController = require('../controllers/timeSlotController');
+const { validateToken }= require('../middleware/auth')
 
-
-router.post('', timeSlotController.createTimeSlot);
-router.get('/timesheet/:id', timeSlotController.getTimeSlots);
-router.get('/:id', timeSlotController.getTimeSlotById);
-router.put('/:id', timeSlotController.updateTimeSlot);
-router.delete('/:id', timeSlotController.deleteTimeSlot);
+router.post('', validateToken, timeSlotController.createTimeSlot);
+router.get('/timesheet/:id', validateToken,  timeSlotController.getTimeSlots);
+router.get('/:id', validateToken, timeSlotController.getTimeSlotById);
+router.put('/:id', validateToken, timeSlotController.updateTimeSlot);
+router.delete('/:id', validateToken, timeSlotController.deleteTimeSlot);
 
 
 module.exports = router;
