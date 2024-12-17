b188c9fd132bc6a54acbda894e9ddf3c513e9170
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-12-17 09:18:16 +0100

    Audit dans expense_report

--- routes/expenseReportRoutes.js
+++ routes/expenseReportRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const expenseReportController = require('../controllers/expenseReportController');
-
+const { validateToken }= require('../middleware/auth');
 const upload = require('../middleware/upload'); 
 
-router.post('', upload.single('document'), expenseReportController.createExpenseReport);
-router.get('/daily/:id', expenseReportController.getExpenseReportsByDailyTimetable);
-router.get('/mensual/:id', expenseReportController.getExpenseReportsByMensualTimetable);
-router.get('/:id', expenseReportController.getExpenseReportById);
-router.put('/:id', expenseReportController.updateExpenseReport);
-router.delete('/:id', expenseReportController.deleteExpenseReport);
+router.post('', validateToken, upload.single('document'), expenseReportController.createExpenseReport);
+router.get('/daily/:id' , validateToken, expenseReportController.getExpenseReportsByDailyTimetable);
+router.get('/mensual/:id' , validateToken, expenseReportController.getExpenseReportsByMensualTimetable);
+router.get('/:id' , validateToken, expenseReportController.getExpenseReportById);
+router.put('/:id' , validateToken, expenseReportController.updateExpenseReport);
+router.delete('/:id' , validateToken, expenseReportController.deleteExpenseReport);
 
 module.exports = router;
